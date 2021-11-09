9c20e8415d52132de4b0d84ca75a4ba83b393c49
Muhammad Gul (s5312271) <s5312271@bournemouth.ac.uk>
2021-11-09 17:33:50 +0000

    added files/folders

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    print("This is an edit")
+    print("This is from a different git branch!")
+    return 'Goodbye World!'
+
+
+
+if __name__ == '__main__':
+    app.run()
