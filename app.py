c90dc3e6d3059bcee2a9bb0b549f79951de50781
Vinnie Fisher <71513636+thurstn@users.noreply.github.com>
2021-11-14 15:34:01 +0000

    added search algorithm
    - added two functions to the python file for recipe search functionality.
    - also modified some other unimportant stuff.

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, make_response
+import sys, json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    print("This is an edit")
-    print("This is from a different git branch!")
     return render_template("website.html")
 
 
@@ -19,6 +18,42 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/recipes', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def recipes():
+    """
+    Functions for the recipes.json file.
+    """
+    with open("data/recipes.json", "r") as file:
+        recipes = file.read()
+    
+@app.route('/search', methods=['GET'])
+def search():
+    """
+    Search the recipes.json file based on user input.
+    """
+    searchRequest = str(request.args.get('searchRequest'))
+    with open("data/recipes.json", "r") as file:
+        searchResults = []
+        recipes = json.load(file).get("recipes")
+        for recipe in recipes:
+            search_loop(searchRequest, searchResults, recipe)
+        file.close()
+    response = make_response(jsonify({"result", searchResults}))
+    response.headers["Content-Type"] = "application/json"
+    return response
+# Nested loops for the searching algorithm. Allows for loop breaking to the outermost loop in the above function.
+def search_loop(searchRequest, searchResults, recipe):
+    if (searchRequest in recipe['title']):
+        searchResults.append(recipe)
+        return
+    for key in recipe['ingredients']:
+        if (searchRequest in key):
+            searchResults.append(recipe)
+            return
+    for tag in recipe['categories']:
+        if (searchRequest in tag):
+            searchResults.append(recipe)
+            return
 
 if __name__ == '__main__':
     app.run(debug=True)
