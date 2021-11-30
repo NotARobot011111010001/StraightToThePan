e7f10fcec49d23e233ecee752a7e4fb4acb8797e
Vinnie Fisher <71513636+thurstn@users.noreply.github.com>
2021-11-30 17:09:23 +0000

    Update functions.js

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -1,11 +1,32 @@
+//*
+  * GetRecipes()
+  * Gets all recipes from the recipes.JSON file as an object.
+*//
 function GetRecipes()
 {
+  let url = "/recipes";
+  let response = "Error while retriving.";
+  let recipesData = "";
+  let recipes = "";
+  let xhttp = new XMLHttpRequest();
   
+  xhttp.onreadystatechange = function() 
+  {
+    if (xhttp.readyState == 4 && xhttp.status == 200)
+    {
+      response = JSON.parse(xhttp.responseText);
+      recipesData = JSON.parse(response.result).recipes;
+      recipes = formatJson(recipesData);
+    }
+  }
+  xhttp.open("GET", url, true);
+  xhttp.send();
 }
+GetRecipes();
 
-//*
-  * Recipe suggestions.
-  * 
+/**
+  * PopulateDiscover()
+  * Gets a specified number of recipes for the discover
 *//
 function PopulateDiscover(int count) // count is number of recipes to get
 {
